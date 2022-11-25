22b483e3d4138403b4dc65dce61b57db22384552
eloygomes <50409591+eloygomes@users.noreply.github.com>
2022-11-25 15:06:25 -0300

    Start to deploy fetching API
    Start of deploying of code to fetching API, but need be revisited

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazemCore';
 
 const Discover = () => {
+    const {data, isFetching, error} = useGetTopChartsQuery;
     const genreTitle = 'Pop';
+
+    console.log(data)
     
     return (
         <div className='flex flex-col'>
@@ -14,7 +18,7 @@ const Discover = () => {
             </div>
             <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
                 {[1,2,3,4,5,6,7,8,9,10].map((song, i)=>(
-                    <SongCard key={song.key} song={song} i={i}/>
+                    <SongCard key={song} song={song} i={i}/>
                 ))}
             </div>
         </div>
@@ -22,3 +26,4 @@ const Discover = () => {
 }
 
 export default Discover;
+
---
+++ src/redux/services/shazemCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+    export const shazamCoreApi = createApi({
+        reducerPath: 'shazamCoreApi',
+        baseQuery: fetchBaseQuery({
+            baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders: (headers)=>{
+                headers.set('X-RapidAPI-Key','397d3acd44msh07433e0783a21d0p19c4b8jsne72b778459cd');
+                return headers;
+            },
+        }),
+        endpoints: (builder)=> ({
+            getTopCharts: builder.query({query: ()=> '/charts/world'}),
+
+        })
+    })
+
+    export const {
+        useGetTopChartsQuery,
+    } = shazamCoreApi;
